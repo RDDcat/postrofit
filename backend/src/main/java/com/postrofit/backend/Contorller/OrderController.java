632c22cbb18f43889eb36c5bf8b907da5ff9c1b7
RDDcat <yukmaro@gmail.com>
2022-11-24 20:33:41 +0900

    [ADD] 컨트롤러 도메인별로 생성

--- backend/src/main/java/com/postrofit/backend/Contorller/OrderController.java
+++ backend/src/main/java/com/postrofit/backend/Contorller/OrderController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SampleController {
+public class OrderController {
     @GetMapping("/")
     public String welcome(){
         return "index.html";
