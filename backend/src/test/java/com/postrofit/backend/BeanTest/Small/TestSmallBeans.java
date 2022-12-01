039ef9dbacc68442e38676f8ab805778d3d468b2
RDDcat <yukmaro@gmail.com>
2022-12-01 21:24:17 +0900

    [ADD] Test용 클래스 추가

---
+++ backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestSmallBeans.java
@@ -0,0 +1,27 @@
+package com.postrofit.backend.BeanTest.Small;
+
+import org.junit.jupiter.api.Test;
+
+
+public class TestSmallBeans {
+    @Test
+    public void testGetOrderBean(){
+
+    }
+    @Test
+    public void testGetStationIdBean(){
+
+    }
+    @Test
+    public void testGetStorageListBean(){
+
+    }
+    @Test
+    public void testGetStoragePasswordBean(){
+
+    }
+    @Test
+    public void testGetStorageStatDTOBean(){
+
+    }
+}
