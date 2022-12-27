8d30eec0c67b5d160c62681eaaa063e6248cddfc
RDDcat <yukmaro@gmail.com>
2022-12-28 01:00:27 +0900

    [UPDATE] 데이터 변경

--- backend/src/main/java/com/postrofit/backend/Bean/Delivery1Bean.java
+++ backend/src/main/java/com/postrofit/backend/Bean/Delivery1Bean.java
@@ -9,8 +9,8 @@ public class Delivery1Bean {
         // TODO 출발역 도착역으로 사이즈별 예상 금액
         // TODO 이거 비용 알고리즘이 없음;;
         DeliveryCostDTO deliveryCostDTO = new DeliveryCostDTO();
-        deliveryCostDTO.setMidCost(4000);
-        deliveryCostDTO.setSmallCost(2500);
+        deliveryCostDTO.setMidCost(1800);
+        deliveryCostDTO.setSmallCost(1200);
 
         return deliveryCostDTO;
     }
