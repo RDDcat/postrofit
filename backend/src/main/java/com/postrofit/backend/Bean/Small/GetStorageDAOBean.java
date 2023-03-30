3bd6e2dec054d7b6fbd36088bb8bad1062142fc4
RDDcat <yukmaro@gmail.com>
2023-03-30 15:35:41 +0900

    [FIX] Store3Bean 에러 수정

--- backend/src/main/java/com/postrofit/backend/Bean/Small/GetStorageDAOBean.java
+++ backend/src/main/java/com/postrofit/backend/Bean/Small/GetStorageDAOBean.java
@@ -2,6 +2,7 @@ package com.postrofit.backend.Bean.Small;
 
 import com.postrofit.backend.Model.DAO.OrderDAO;
 import com.postrofit.backend.Model.DAO.StorageDAO;
+import com.postrofit.backend.Model.DAO.StoreDAO;
 import com.postrofit.backend.Repository.StorageDAORepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -19,5 +20,9 @@ public class GetStorageDAOBean {
 
     public StorageDAO exec(OrderDAO order) {
         return storageRepository.findStorageDAOByStorageId(order.getStorageId());
+    }
+
+    public StorageDAO exec(StoreDAO storeDAO) {
+        return storageRepository.findStorageDAOByStorageId(storeDAO.getStorageId());
     }
 }
