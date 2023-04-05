6f897bdea94b593b8fa892ff696d9beff0a984ac
yukmaro <2018158018@tukorea.ac.kr>
2023-04-05 14:56:14 +0900

    [ADD] 테스트 코드 추가

--- backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestSmallBeans.java
+++ backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestSmallBeans.java
@@ -1,41 +1,41 @@
 package com.postrofit.backend.BeanTest.Small;
 
 import com.postrofit.backend.Bean.Small.*;
+import com.postrofit.backend.Model.DAO.OrderDAO;
+import com.postrofit.backend.Model.DAO.UserDAO;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class TestSmallBeans {
     @Autowired
-    GetStationIdBean getStationIdBean;
-    @Autowired
-    GetStorageListBean getStorageListBean;
-    @Autowired
-    GetStorageStatDTOBean getStorageStatDTOBean;
-    @Autowired
     GetOrderDAOBean getOrderDAOBean;
-    @Autowired
-    GetStoragePasswordBean getStoragePasswordBean;
 
 
     @Test
-    public void testGetOrderDAOBean(){
+    @DisplayName("testGetOrderDAOBean 테스트 : UserDAO")
+    void testGetOrderDAOBean_1(){
+        UserDAO userDAO = new UserDAO();
+        OrderDAO expect = new OrderDAO();
 
+        OrderDAO orderDAO = getOrderDAOBean.exec(userDAO);
+
+        assertEquals(expect, orderDAO);
     }
-    @Test
-    public void testGetStationIdBean(){
 
-    }
     @Test
-    public void testGetStorageListBean(){
+    @DisplayName("testGetOrderDAOBean 테스트 : String start, String end, String userId")
+    void testGetOrderDAOBean_2(){
+        String start = "";
+        String end = "";
+        String userId = "";
+        OrderDAO expect = new OrderDAO();
 
-    }
-    @Test
-    public void testGetStoragePasswordBean(){
+        OrderDAO orderDAO = getOrderDAOBean.exec(start, end, userId);
 
-    }
-    @Test
-    public void testGetStorageStatDTOBean(){
-
+        assertEquals(expect, orderDAO);
     }
 }
