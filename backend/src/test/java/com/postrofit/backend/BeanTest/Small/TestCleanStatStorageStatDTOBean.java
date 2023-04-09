e4d1fc8eb4b8fb4be7f288ad0bf324cb4c0fa099
euihyuneee <lobil7782@gmail.com>
2023-04-09 11:49:49 +0900

    [ADD] TestCleanStatStorageSTatDTOBean 작성

--- backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestCleanStatStorageStatDTOBean.java
+++ backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestCleanStatStorageStatDTOBean.java
@@ -1,10 +1,22 @@
 package com.postrofit.backend.BeanTest.Small;
 
 import com.postrofit.backend.Bean.Small.CleanStatStorageStatDTOBean;
+import com.postrofit.backend.Model.DTO.StorageStatDTO;
+import com.postrofit.backend.Model.Enum.StorageSize;
+import com.postrofit.backend.Model.Enum.StorageStat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import java.util.ArrayList;
+import java.util.List;
 
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class TestCleanStatStorageStatDTOBean {
 
     @Autowired
@@ -12,7 +24,19 @@ public class TestCleanStatStorageStatDTOBean {
 
     @Test
     @DisplayName("testCleanStatStorageStatDTOBean 테스트 : List<StorageStatDTO> storageStatDTOS")
-    void testCleanStatStorageStatDTOBean(){
+    void testCleanStatStorageStatDTOBean() {
+        List<StorageStatDTO> storageStatDTOList = new ArrayList<>();
 
+        for (int i = 0; i < 10; i++) {
+            StorageStatDTO storageStatDTO = new StorageStatDTO();
+            storageStatDTO.setStorageId(i);
+            storageStatDTO.setStorageNumber(1);
+            storageStatDTO.setStorageStat(StorageStat.STORE);
+            storageStatDTO.setStorageSize(StorageSize.BIG);
+            storageStatDTOList.add(storageStatDTO);
+        }
+        List<StorageStatDTO> storageStatDTOList2 = cleanStatStorageStatDTOBean.exec(storageStatDTOList);
+
+        assertEquals(storageStatDTOList, storageStatDTOList2);
     }
 }
