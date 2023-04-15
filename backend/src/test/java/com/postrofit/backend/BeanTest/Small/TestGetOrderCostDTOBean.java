c2742cb5759c372301faf557cd4a5ed1d43cdf07
euihyuneee <lobil7782@gmail.com>
2023-04-15 18:55:25 +0900

    [ADD] TestGetOrderCostDTOBean 테스트 완료

--- backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestGetOrderCostDTOBean.java
+++ backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestGetOrderCostDTOBean.java
@@ -1,10 +1,17 @@
 package com.postrofit.backend.BeanTest.Small;
 
 import com.postrofit.backend.Bean.Small.GetOrderCostDTOBean;
+import com.postrofit.backend.Model.DTO.OrderCostDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class TestGetOrderCostDTOBean {
 
     @Autowired
@@ -13,7 +20,13 @@ public class TestGetOrderCostDTOBean {
     @Test
     @DisplayName("testGetOrderCostDTOBean 테스트 : String start, String end")
     void testGetOrderCostDTOBean(){
+        String start = "test1";
+        String end = "test2";
+        int smallCost = 2000;
 
+        OrderCostDTO orderCostDTO = getOrderCostDTOBean.exec(start, end);
+
+        assertEquals(smallCost, orderCostDTO.getSmallCost());
     }
 
 }
