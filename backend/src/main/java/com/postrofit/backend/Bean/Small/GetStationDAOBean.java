8d9d646fbdffff90418d13370f7a9f6b4f6ac445
euihyuneee <lobil7782@gmail.com>
2023-04-15 19:24:11 +0900

    [ADD] TestGetStationDAOBean 테스트 완료

--- backend/src/main/java/com/postrofit/backend/Bean/Small/GetStationDAOBean.java
+++ backend/src/main/java/com/postrofit/backend/Bean/Small/GetStationDAOBean.java
@@ -12,14 +12,17 @@ public class GetStationDAOBean {
     @Autowired
     StationDAORepository stationRepository;
 
+    // TODO name(역 이름)을 가지고 StationDAO를 가져오기
     public StationDAO exec(String name) {
         return stationRepository.findByStationName(name);
     }
 
+    // TODO 역 ID를 가지고 StationDAO를 가져오기
     public StationDAO exec(long stationId) {
         return stationRepository.findById(stationId).get();
     }
 
+    // TODO StorageDAO(보관함 정보)를 가지고 보관함이 있는 역(StationDAO)정보 가져오기
     public StationDAO exec(StorageDAO storageDAO) {
         return stationRepository.findById(storageDAO.getStationId()).get();
     }
--- backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestGetStationDAOBean.java
+++ backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestGetStationDAOBean.java
@@ -1,10 +1,24 @@
 package com.postrofit.backend.BeanTest.Small;
 
 import com.postrofit.backend.Bean.Small.GetStationDAOBean;
+import com.postrofit.backend.Model.DAO.StationDAO;
+import com.postrofit.backend.Model.DAO.StorageDAO;
+import com.postrofit.backend.Model.Enum.StorageSize;
+import com.postrofit.backend.Model.Enum.StorageStat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class TestGetStationDAOBean {
 
     @Autowired
@@ -13,17 +27,30 @@ public class TestGetStationDAOBean {
     @Test
     @DisplayName("testGetStationDAOBean_1 테스트 : String name")
     void testGetStationDAOBean_1(){
+        String name = "테스트역1";
 
+        StationDAO stationDAO = getStationDAOBean.exec(name);
+
+        assertEquals(name, stationDAO.getStationName());
     }
     @Test
     @DisplayName("testGetStationDAOBean_2 테스트 : long stationId")
     void testGetStationDAOBean_2(){
+        long stationId = 1L;
 
+        StationDAO stationDAO = getStationDAOBean.exec(stationId);
+
+        assertEquals(stationId, stationDAO.getStationId());
     }
     @Test
     @DisplayName("testGetStationDAOBean_3 테스트 : StorageDAO storageDAO")
     void testGetStationDAOBean_3(){
+        Date date = new Date();
+        StorageDAO storageDAO = new StorageDAO();
 
+        StationDAO stationDAO = getStationDAOBean.exec(storageDAO);
+
+        assertEquals(storageDAO.getStationId(), stationDAO.getStationId());
     }
     @Test
     @DisplayName("testGetStationDAOBean_4 테스트 : RequestMakeOrderDTO requestMakeOrderDTO")
