58faefaf47ddc6683308de9c431fe2ff5d1fc872
euihyuneee <lobil7782@gmail.com>
2023-04-18 15:26:09 +0900

    [ADD] TestGetUserDAOBean 테스트 완료

--- backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestGetUserDAOBean.java
+++ backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestGetUserDAOBean.java
@@ -1,6 +1,9 @@
 package com.postrofit.backend.BeanTest.Small;
 
 import com.postrofit.backend.Bean.Small.GetUserDAOBean;
+import com.postrofit.backend.Model.DAO.UserDAO;
+import com.postrofit.backend.Model.DTO.RequestMakeOrderDTO;
+import com.postrofit.backend.Model.DTO.RequestStoreDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,16 +24,31 @@ public class TestGetUserDAOBean {
     @Test
     @DisplayName("testGetUserDAOBean_1 테스트 : long userId")
     void testGetUserDAOBean_1(){
+        // userId -> "마로아님"
+        long userId = 0L;
 
+        UserDAO userDAO = getUserDAOBean.exec(userId);
+
+        assertEquals(userId, userDAO.getUserId());
     }
     @Test
     @DisplayName("testGetUserDAOBean_2 테스트 : RequestMakeOrderDTO requestMakeOrderDTO")
     void testGetUserDAOBean_2(){
+        RequestMakeOrderDTO requestMakeOrderDTO = new RequestMakeOrderDTO();
+        requestMakeOrderDTO.setUserId(0L);
 
+        UserDAO userDAO = getUserDAOBean.exec(requestMakeOrderDTO);
+
+        assertEquals(requestMakeOrderDTO.getUserId(), userDAO.getUserId());
     }
     @Test
     @DisplayName("testGetUserDAOBean_3 테스트 : RequestStoreDTO requestStoreDTO")
     void testGetUserDAOBean_3(){
+        RequestStoreDTO requestStoreDTO = new RequestStoreDTO();
+        requestStoreDTO.setUserId(0L);
 
+        UserDAO userDAO = getUserDAOBean.exec(requestStoreDTO);
+
+        assertEquals(requestStoreDTO.getUserId(), userDAO.getUserId());
     }
 }
