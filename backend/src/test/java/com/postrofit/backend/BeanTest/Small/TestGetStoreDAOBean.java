855cbadea2347690bf8f6ec93f4b42fa34ffdf4c
euihyuneee <lobil7782@gmail.com>
2023-04-18 15:05:50 +0900

    [ADD] TestGetStoreDAOBean 테스트 완료

--- backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestGetStoreDAOBean.java
+++ backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestGetStoreDAOBean.java
@@ -1,6 +1,7 @@
 package com.postrofit.backend.BeanTest.Small;
 
 import com.postrofit.backend.Bean.Small.GetStoreDAOBean;
+import com.postrofit.backend.Model.DAO.StoreDAO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,8 +20,22 @@ public class TestGetStoreDAOBean {
     GetStoreDAOBean getStoreDAOBean;
 
     @Test
-    @DisplayName("testGetStoreDAOBean 테스트 : String userId")
-    void testGetStoreDAOBean(){
+    @DisplayName("testGetStoreDAOBean_0 테스트 : String userId")
+    void testGetStoreDAOBean_0(){
+        // 이건 왜 있는 걸까?
+        String userId = "0";
 
+        StoreDAO storeDAO = getStoreDAOBean.exec(userId);
+
+        assertEquals(Long.parseLong(userId), storeDAO.getUserId());
+    }
+    @Test
+    @DisplayName("testGetStoreDAOBean 테스트 : long userId")
+    void testGetStoreDAOBean_1(){
+        long userId = 0L;
+
+        StoreDAO storeDAO = getStoreDAOBean.exec(userId);
+
+        assertEquals(userId, storeDAO.getUserId());
     }
 }
