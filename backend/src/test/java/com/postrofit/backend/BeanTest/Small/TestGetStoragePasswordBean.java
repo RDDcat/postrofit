9b72bb79768e2cf3ffee57a98d26f80f3779e39c
euihyuneee <lobil7782@gmail.com>
2023-04-18 13:21:46 +0900

    [ADD] TestGetStoragePasswordBean 테스트 완료

--- backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestGetStoragePasswordBean.java
+++ backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestGetStoragePasswordBean.java
@@ -2,6 +2,9 @@ package com.postrofit.backend.BeanTest.Small;
 
 import com.netflix.discovery.converters.Auto;
 import com.postrofit.backend.Bean.Small.GetStoragePasswordBean;
+import com.postrofit.backend.Model.DAO.StorageDAO;
+import com.postrofit.backend.Model.DTO.StoragePasswordDTO;
+import com.postrofit.backend.Repository.StorageDAORepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,10 +21,18 @@ public class TestGetStoragePasswordBean {
 
     @Autowired
     GetStoragePasswordBean getStoragePasswordBean;
+    @Autowired
+    StorageDAORepository storageDAORepository;
 
     @Test
     @DisplayName("testGetStoragePasswordBean 테스트 : StorageDAO storageDAO")
     void testGetStoragePasswordBean(){
+        // TODO StoragePasswordDTO를 만들기 위함
+        // TODO StorageDAO에서 StationDAO, StoragePasswordDAO를 만듬
+        StorageDAO storageDAO = storageDAORepository.findStorageDAOByStorageId(0L);
 
+        StoragePasswordDTO storagePasswordDTO = getStoragePasswordBean.exec(storageDAO);
+
+        assertEquals(storageDAO.getStorageNumber(), storagePasswordDTO.getStorageNum());
     }
 }
