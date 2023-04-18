f72f92c458e4a7ac202cde6616bc3319461712c0
euihyuneee <lobil7782@gmail.com>
2023-04-18 13:16:17 +0900

    [ADD] TestGetStorageListBean 테스트 완료 및 남은 테스트에 어노테이션 추가

--- backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestGetStoragePasswordBean.java
+++ backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestGetStoragePasswordBean.java
@@ -4,8 +4,16 @@ import com.netflix.discovery.converters.Auto;
 import com.postrofit.backend.Bean.Small.GetStoragePasswordBean;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class TestGetStoragePasswordBean {
 
     @Autowired
--- backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestSaveOrderBean.java
+++ backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestSaveOrderBean.java
@@ -3,8 +3,16 @@ package com.postrofit.backend.BeanTest.Small;
 import com.postrofit.backend.Bean.Small.SaveOrderBean;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class TestSaveOrderBean {
 
     @Autowired
