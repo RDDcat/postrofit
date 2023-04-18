25bfe8cae6e8c5da52a042747b68bfff7c31eed4
euihyuneee <lobil7782@gmail.com>
2023-04-18 15:31:06 +0900

    [ADD] TestGetWaitingOrderBean 테스트 완료

--- backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestGetWaitingOrderBean.java
+++ backend/src/test/java/com/postrofit/backend/BeanTest/Small/TestGetWaitingOrderBean.java
@@ -1,6 +1,9 @@
 package com.postrofit.backend.BeanTest.Small;
 
 import com.postrofit.backend.Bean.Small.GetWaitingOrderBean;
+import com.postrofit.backend.Model.DAO.OrderDAO;
+import com.postrofit.backend.Model.DAO.StationDAO;
+import com.postrofit.backend.Repository.StationDAORepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,10 +20,19 @@ public class TestGetWaitingOrderBean {
 
     @Autowired
     GetWaitingOrderBean getWaitingOrderBean;
+    @Autowired
+    StationDAORepository stationDAORepository;
 
     @Test
     @DisplayName("testGetWaitingOrderBean 테스트 : StationDAO start, StationDAO end")
     void testGetWaitingOrderBean(){
+        // init 데이터의 OrderDAO order1 참고
+        StationDAO start = stationDAORepository.findByStationName("테스트역0");
+        StationDAO end = stationDAORepository.findByStationName("테스트역2");
 
+        OrderDAO orderDAO = getWaitingOrderBean.exec(start, end);
+
+        assertEquals(start.getStationId(), orderDAO.getStartStationId());
+        assertEquals(end.getStationId(), orderDAO.getEndStationId());
     }
 }
