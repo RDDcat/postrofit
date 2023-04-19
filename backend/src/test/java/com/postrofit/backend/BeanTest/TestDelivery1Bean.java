64f95a6ec1d76065a92cd1f9e23974ee3eacc78c
euihyuneee <lobil7782@gmail.com>
2023-04-19 18:07:31 +0900

    [ADD] Delivery1Bean 테스트 완료. [구현 내용이 없음. 테스트 코드 만들어 두기만 함]

--- backend/src/test/java/com/postrofit/backend/BeanTest/TestDelivery1Bean.java
+++ backend/src/test/java/com/postrofit/backend/BeanTest/TestDelivery1Bean.java
@@ -1,6 +1,9 @@
 package com.postrofit.backend.BeanTest;
 
 import com.postrofit.backend.Bean.Delivery1Bean;
+import com.postrofit.backend.Model.DTO.DeliveryCostDTO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,4 +16,16 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestDelivery1Bean {
     @Autowired
     Delivery1Bean delivery1Bean;
+
+    @Test
+    @DisplayName("Delivery1Bean 테스트 : String start, String end")
+    void testDelivery1Bean(){
+        // TODO 구현 덜 됨. start랑 end 쓰질 않음.
+        String start = "테스트역1";
+        String end = "테스트역2";
+
+        DeliveryCostDTO deliveryCostDTO = delivery1Bean.exec(start, end);
+
+        assertEquals(1800, deliveryCostDTO.getMidCost());
+    }
 }
