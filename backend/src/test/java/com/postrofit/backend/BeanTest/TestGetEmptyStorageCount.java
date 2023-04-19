8b6463c0befa6d409e2be7dfc5936032985a9a8f
euihyuneee <lobil7782@gmail.com>
2023-04-19 18:51:05 +0900

    [ADD] GetEmptyStorageCount 테스트 완료

--- backend/src/test/java/com/postrofit/backend/BeanTest/TestGetEmptyStorageCount.java
+++ backend/src/test/java/com/postrofit/backend/BeanTest/TestGetEmptyStorageCount.java
@@ -1,5 +1,8 @@
 package com.postrofit.backend.BeanTest;
 import com.postrofit.backend.Bean.GetEmptyStorageCount;
+import com.postrofit.backend.Model.DTO.EmptyStorageCountDTO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,4 +15,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestGetEmptyStorageCount {
     @Autowired
     GetEmptyStorageCount getEmptyStorageCount;
+
+    @Test
+    @DisplayName("GetEmptyStorageCount 테스트 : String stationName")
+    void testGetEmptyStorageCount() {
+        String stationName = "테스트역1";
+
+        EmptyStorageCountDTO emptyStorageCountDTO = getEmptyStorageCount.exec(stationName);
+
+        assertEquals(6, emptyStorageCountDTO.getSmallCount());
+    }
 }
